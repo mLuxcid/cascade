a60842e54817d582a254950d2ead97f9773641e7
luxcid <120787347+mLuxcid@users.noreply.github.com>
2024-05-22 12:34:03 +0200

    Do not the pointers! (p.1)

--- src/window.h
+++ src/window.h
@@ -7,10 +7,10 @@
 struct window_t;
 
 // create a window with the width of `w`, the height of `h` and a title set
-void window_create(struct window_t *window, size_t w, size_t h, const char *title);
+void window_create(struct window_t **window, size_t w, size_t h, const char *title);
 
 bool window_should_close(struct window_t *window);
-void window_set_should_close(struct window_t *window, bool value);
+void window_set_should_close(struct window_t **window, bool value);
 
 // destroy the window & clean up nessecary resources
-void window_delete(struct window_t *window);
+void window_delete(struct window_t **window);
