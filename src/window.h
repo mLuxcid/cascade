55d243bbadece745ab9afbd264a80fd2a0231d10
luxcid <120787347+mLuxcid@users.noreply.github.com>
2024-05-22 12:22:24 +0200

    Coding style

--- src/window.h
+++ src/window.h
@@ -3,22 +3,10 @@
 #include <GLFW/glfw3.h>
 #include <stddef.h>
 
-typedef struct window {
-    // GLFW window
-    GLFWwindow *window;
-
-    // size
-    size_t width;
-    size_t height;
-
-    // title
-    const char *title;
-
-    int should_close;
-} Window;
+struct window_t;
 
 // create a window with the width of `w`, the height of `h` and a title set
-void window_create(Window *window, size_t w, size_t h, const char *title);
+void window_create(struct window_t *window, size_t w, size_t h, const char *title);
 
 // destroy the window & clean up nessecary resources
-void window_delete(Window *window);
+void window_delete(struct window_t *window);
